3e34fa41effc8d6273f16458d1bde8f3389df5c6
Imre Ferenczi <enjoy_sicilia@live.fr>
2026-04-13 15:15:06 +0200

    Update EpubReaderActivity.h

--- src/activities/reader/EpubReaderActivity.h
+++ src/activities/reader/EpubReaderActivity.h
@@ -27,6 +27,8 @@ class EpubReaderActivity final : public Activity {
   bool pendingScreenshot = false;
   bool skipNextButtonCheck = false;  // Skip button processing for one frame after subactivity exit
   bool automaticPageTurnActive = false;
+  private:
+   ReadingControlsMod _readingCtrlMod;
 
   // Footnote support
   std::vector<FootnoteEntry> currentPageFootnotes;
